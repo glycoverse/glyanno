e1b403c24f312a440a7c4bf57d90ff9fba202663
fubin1999 <65430559+fubin1999@users.noreply.github.com>
2025-11-19 16:22:53 +0800

    feat: add enhance_comp

---
+++ R/enhance-comp.R
@@ -0,0 +1,74 @@
+#' Enhance glycan composition
+#'
+#' Given a generic glycan composition (e.g. Hex(5)HexNAc(2)),
+#' this function gives all possible concrete glycan compositions (e.g. Man(5)GlcNAc(2)).
+#'
+#' @param comps A [glyrepr::glycan_composition()] vector,
+#'   or a character vector of glycan composition strings of Byonic or simple style
+#'   (e.g. "Hex(5)HexNAc(2)", "H5N4F1S1").
+#'   Generic compositions (e.g. Hex(5)HexNAc(2)) will be matched to all possible concrete compositions in `db`.
+#'   Concrete compositions (e.g. Man(5)GlcNAc(2)) will be returned as is.
+#' @param db A [glydb::glydb_compositions()] vector,
+#'   or a character vector of glycan composition strings of Byonic or simple style
+#'   (e.g. "Man(5)GlcNAc(2)", "H5N4F1S1").
+#'   All compositions in `db` must be concrete (e.g. Man(5)GlcNAc(2)).
+#'   If not provided, `glydb::glydb_compositions(mono_type = "concrete")` will be used.
+#'
+#' @returns A tibble with the following columns:
+#'   - `raw`: The original compositions.
+#'   - `enhanced`: The enhanced compositions.
+#'     Note that one `raw` composition can have different `enhanced` compositions
+#'     as multiple rows in the result.
+#'
+#' @examples
+#' enhance_comp("Hex(5)HexNAc(2)")
+#'
+#' @export
+enhance_comp <- function(comps, db = NULL) {
+  # Input validation and preparation
+  comps <- .ensure_glycan_composition(comps, allow_structure = FALSE)
+  if (is.null(db)) {
+    db <- glydb::glydb_compositions(mono_type = "concrete")
+  } else {
+    db <- .ensure_glycan_composition(db, allow_structure = FALSE)
+  }
+  db <- unique(db)
+  if (any(glyrepr::get_mono_type(db) == "generic")) {
+    cli::cli_warn("Some compositions in `db` are generic, which will be dropped.")
+    db <- db[glyrepr::get_mono_type(db) == "concrete"]
+  }
+
+  # Prepare tibbles for matching
+  db_df <- tibble::tibble(
+    generic = glyrepr::convert_to_generic(db),
+    concrete = db
+  )
+  comps_df <- tibble::tibble(
+    composition = comps,
+    mono_type = glyrepr::get_mono_type(comps),
+    row_id = seq_along(comps)  # for ordering the result
+  )
+
+  # Process generic compositions
+  generic_res <- comps_df |>
+    dplyr::filter(.data$mono_type == "generic") |>
+    dplyr::inner_join(db_df, by = c("composition" = "generic")) |>
+    dplyr::select(all_of(c("raw" = "composition", "enhanced" = "concrete", "row_id")))
+
+  # Process concrete compositions
+  concrete_res <- comps_df |>
+    dplyr::filter(.data$mono_type == "concrete") |>
+    dplyr::mutate(enhanced = .data$composition) |>
+    dplyr::select(all_of(c("raw" = "composition", "enhanced", "row_id")))
+
+  # Combine the results
+  res <- dplyr::bind_rows(generic_res, concrete_res) |>
+    dplyr::arrange(.data$row_id) |>
+    dplyr::select(all_of(c("raw", "enhanced")))
+
+  # Ensure zero-row result has the expected glycan vector types
+  if (nrow(res) == 0) {
+    res <- tibble::tibble(raw = glyrepr::glycan_composition(), enhanced = glyrepr::glycan_composition())
+  }
+  res
+}
---
+++ tests/testthat/test-enhance-comp.R
@@ -0,0 +1,43 @@
+test_that("enhance_comp works for a basic case", {
+  comps <- glyrepr::glycan_composition(c(Hex = 1, HexNAc = 1))
+  db <- glyrepr::glycan_composition(c(Gal = 1, GalNAc = 1), c(Glc = 1, GalNAc = 1))
+  result <- enhance_comp(comps, db)
+  expected <- tibble::tibble(raw = comps, enhanced = db)
+  expect_equal(result, expected)
+})
+
+test_that("enhance_comp accepts character input", {
+  comps <- "Hex(1)HexNAc(1)"
+  db <- c("Gal(1)GalNAc(1)", "Glc(1)GalNAc(1)")
+  result <- enhance_comp(comps, db)
+  expected <- tibble::tibble(
+    raw = glyrepr::glycan_composition(c(Hex = 1, HexNAc = 1)),
+    enhanced = glyrepr::glycan_composition(c(Gal = 1, GalNAc = 1), c(Glc = 1, GalNAc = 1))
+  )
+  expect_equal(result, expected)
+})
+
+test_that("enhance_comp works with empty input", {
+  comps <- glyrepr::glycan_composition()
+  db <- glyrepr::glycan_composition(c(Gal = 1, GalNAc = 1), c(Glc = 1, GalNAc = 1))
+  result <- enhance_comp(comps, db)
+  expected <- tibble::tibble(raw = glyrepr::glycan_composition(), enhanced = glyrepr::glycan_composition())
+  expect_equal(result, expected)
+})
+
+test_that("enhance_comp works with both generic and concrete compositions", {
+  comps <- c("Hex(1)HexNAc(1)", "Gal(1)GalNAc(1)")
+  db <- c("Glc(1)GalNAc(1)", "Glc(1)GlcNAc(1)")
+  result <- enhance_comp(comps, db)
+  expected <- tibble::tibble(
+    raw = glyrepr::as_glycan_composition(c("Hex(1)HexNAc(1)", "Hex(1)HexNAc(1)", "Gal(1)GalNAc(1)")),
+    enhanced = glyrepr::as_glycan_composition(c("Glc(1)GalNAc(1)", "Glc(1)GlcNAc(1)", "Gal(1)GalNAc(1)"))
+  )
+  expect_equal(result, expected)
+})
+
+test_that("enhance_comp issues a warning when some compositions in db are generic", {
+  comps <- "Hex(1)HexNAc(1)"
+  db <- glyrepr::as_glycan_composition(c("Hex(1)HexNAc(1)", "Gal(1)GalNAc(1)"))
+  expect_warning(enhance_comp(comps, db), "Some compositions in `db` are generic, which will be dropped.")
+})
