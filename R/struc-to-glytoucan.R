5be9af3baf6e7956b75d56c48aa43ad386807efd
fubin1999 <65430559+fubin1999@users.noreply.github.com>
2026-04-29 18:29:27 +0800

    fix: encode characters like ? in urls

--- R/struc-to-glytoucan.R
+++ R/struc-to-glytoucan.R
@@ -18,11 +18,14 @@ struc_to_glytoucan <- function(strucs) {
   }
 
   base_url <- "https://api.glycosmos.org/glycanformatconverter/2.8.2/iupaccondensed2wurcs/"
-  urls <- paste0(base_url, iupacs)
+  encoded_iupacs <- purrr::map_chr(iupacs, utils::URLencode, reserved = TRUE, repeated = TRUE)
+  urls <- paste0(base_url, encoded_iupacs)
+
   reqs <- purrr::map(urls, httr2::request) |>
     purrr::map(httr2::req_throttle, capacity = 60) |>
     purrr::map(httr2::req_retry, max_tries = 2)
   resps <- purrr::map(reqs, httr2::req_perform)
+
   purrr::map_chr(resps, function(resp) {
     if (httr2::resp_status(resp) == 200) {
       content <- httr2::resp_body_json(resp)
