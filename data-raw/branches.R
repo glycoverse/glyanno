5c68035d407df4f29a40897c045f4b095e0c2fa0
fubin1999 <65430559+fubin1999@users.noreply.github.com>
2026-07-19 15:54:22 +0800

    feat: add internal branch motif data

---
+++ data-raw/branches.R
@@ -0,0 +1,31 @@
+# This script extracts topological and intact N-glycan branching motifs
+# from `glydb::glydb_structures()`.
+
+library(tidyverse)
+library(glyrepr)
+library(glymotif)  # 0.17.3
+library(glydb)  # 0.6.0
+
+# Extract branches motifs with confidence
+extract_branches <- function(glycans) {
+  branch_mat <- have_motifs(glycans, branch_motifs())
+  branch_data <- branch_mat |>
+    as.data.frame() |>
+    rownames_to_column("glycan") |>
+    as_tibble() |>
+    mutate(glycan_confidence = attr(glycans, "confidence"), .after = glycan) |>
+    pivot_longer(-c(glycan, glycan_confidence), names_to = "motif", values_to = "has") |>
+    filter(has) |>
+    summarise(confidence = max(glycan_confidence), .by = motif)
+  branches <- as_glycan_structure(branch_data$motif)
+  attr(branches, "confidence") <- branch_data$confidence
+  branches
+}
+
+intact_glycans <- glydb_structures(structure_level = "intact", glycan_type = "N")
+topological_glycans <- glydb_structures(structure_level = "topological", glycan_type = "N")
+
+intact_branches <- extract_branches(intact_glycans)
+topological_branches <- extract_branches(topological_glycans)
+
+usethis::use_data(intact_branches, topological_branches, internal = TRUE, overwrite = TRUE)
